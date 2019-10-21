ac55490d8276a712498039d19116e939ee892690
rigoneri <rodrigo@rigoneri.com>
2019-10-20 21:02:25 -0500

    Small change to use context hook instead of context consumer hoc

--- src/pages/Spotify/RecentTracks.js
+++ src/pages/Spotify/RecentTracks.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import { parseISO, formatDistanceToNow } from 'date-fns'
 import Img from 'react-image'
 import { PlayContext } from './PlayContext'
@@ -8,6 +8,7 @@ import styles from './Spotify.module.css'
 export default function RecentTracks() {
     const [tracks, setTracks] = useState([])
     const [error, setError] = useState(false)
+    const context = useContext(PlayContext)
 
     const fetchTracks = async () => {
         try {
@@ -24,43 +25,36 @@ export default function RecentTracks() {
     }, [])
 
     return (
-        <PlayContext.Consumer>
-            {context => (
-                <div className={styles.recentTracks}>
-                    <h3>Recent Tracks</h3>
-                    {error && <p className={styles.error}>Unable to fetch recent tracks.</p>}
-                    {tracks && tracks.length ? (
-                        <ul>
-                            {tracks.map(track => (
-                                <li key={`${track.id}-${track.date}`}>
-                                    <a
-                                        href={track.url}
-                                        className={styles.album}
-                                        onClick={e => {
-                                            e.preventDefault()
-                                            context.onPlayTrack(track)
-                                        }}>
-                                        <Img src={track.image} alt={track.title} />
-                                        {track.preview_url && (
-                                            <span
-                                                className={`${styles.playIcon} ${
-                                                    context.playing && context.playing === track.id ? styles.playing : ''
-                                                }`}>
-                                                {context.playing && context.playing === track.id ? <PauseLogo /> : <PlayLogo />}
-                                            </span>
-                                        )}
-                                    </a>
-                                    <a href={track.url} className={styles.title}>
-                                        {track.title}
-                                    </a>
-                                    <span className={styles.artist}>{track.artist}</span>
-                                    <span className={styles.date}>{formatDistanceToNow(parseISO(track.date))} ago</span>
-                                </li>
-                            ))}
-                        </ul>
-                    ) : null}
-                </div>
-            )}
-        </PlayContext.Consumer>
+        <div className={styles.recentTracks}>
+            <h3>Recent Tracks</h3>
+            {error && <p className={styles.error}>Unable to fetch recent tracks.</p>}
+            {tracks && tracks.length ? (
+                <ul>
+                    {tracks.map(track => (
+                        <li key={`${track.id}-${track.date}`}>
+                            <a
+                                href={track.url}
+                                className={styles.album}
+                                onClick={e => {
+                                    e.preventDefault()
+                                    context.onPlayTrack(track)
+                                }}>
+                                <Img src={track.image} alt={track.title} />
+                                {track.preview_url && (
+                                    <span className={`${styles.playIcon} ${context.playing && context.playing === track.id ? styles.playing : ''}`}>
+                                        {context.playing && context.playing === track.id ? <PauseLogo /> : <PlayLogo />}
+                                    </span>
+                                )}
+                            </a>
+                            <a href={track.url} className={styles.title}>
+                                {track.title}
+                            </a>
+                            <span className={styles.artist}>{track.artist}</span>
+                            <span className={styles.date}>{formatDistanceToNow(parseISO(track.date))} ago</span>
+                        </li>
+                    ))}
+                </ul>
+            ) : null}
+        </div>
     )
 }
